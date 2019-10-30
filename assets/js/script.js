a2fede016bc787df80f5a37841079cddb7b9ed38
Francesco <franc.ns@gmail.com>
2019-10-30 14:12:21 +0100

    added first part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 // - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso il for in tutte le proprietà.
-// - Creare un array di oggetti di studenti.
+
+// creazione oggetto
+var firstStudent = {
+  'nome' : 'Francesco',
+  'cognome' : 'Biava',
+  'età' : '23',
+};
+
+// ciclo for in con output
+for (var key in firstStudent) {
+  console.log(key + ': ' + firstStudent[key]);
+}
+
+
+
+// Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+
+
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
